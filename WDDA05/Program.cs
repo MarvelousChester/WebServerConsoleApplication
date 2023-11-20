2ebf683cb1cce70f21cef9a55107c9e7abb9edfb
MarvelousChester <karandeepsandhu2789@gmail.com>
2023-11-20 15:32:10 -0500

    Initial commit

---
+++ WDDA05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WDDA05
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// Get Args
+			const int MIN_ARGS = 2;
+
+			Console.WriteLine(args.Length);
+
+
+		}
+	}
+}
