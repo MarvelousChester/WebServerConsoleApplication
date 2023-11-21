722149a8f5e505d9c9bbbe8724d85a1075ad149c
MarvelousChester <karandeepsandhu2789@gmail.com>
2023-11-21 15:01:01 -0500

    Add Reading Args

--- A05WDD/Program.cs
+++ A05WDD/Program.cs
@@ -11,8 +11,35 @@ namespace A05WDD
 	{
 		static void Main(string[] args)
 		{
+			// Accept Args
+			const int MIN_ARGS = 3;
+			string webRoot = "";
+			string ip;
+			string port;
 
+			// Validate Length and extract 3 args given
+			// –webRoot=C:\localWebSite –webIP=192.168.100.23 –webPort=5300
+			if (args.Length != MIN_ARGS)
+			{
 
+				Console.WriteLine("Invalid Amount of Args Provided");
+				Environment.Exit(0);
+			}
+
+			// PArse
+			const int WEBROOT_START_INDEX = 9;
+			const int IP_START_INDEX = 7;
+			const int PORT_START_INDEX = 9;
+
+			webRoot = args[0];
+			webRoot = webRoot.Trim().Substring(WEBROOT_START_INDEX);
+			Console.WriteLine(webRoot);
+			ip = args[1];
+			ip = ip.Trim().Substring(IP_START_INDEX);
+			Console.WriteLine(ip);
+			port = args[2];
+			port = port.Trim().Substring(PORT_START_INDEX);
+			Console.WriteLine(port);
 		}
 	}
 }
