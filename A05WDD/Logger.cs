38df2a502934408d31d46c64abe0038797fd344f
MarvelousChester <karandeepsandhu2789@gmail.com>
2023-11-20 19:25:17 -0500

    Add Logger

--- A05WDD/Logger.cs
+++ A05WDD/Logger.cs
@@ -7,29 +7,46 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-
 namespace A05WDD
 {
 	public static class Logger 
 	{
+
+		/// <summary>
+		/// Gets File Directory path to project folder, gets message and type of log and logs into file with date
+		/// </summary>
+		/// <param name="message">Message that needs to be logged</param>
+		/// <param name="logType">The [LogType] for the message</param>
+		public static void WriteLog(string message, string logType) {
+
+			try
+			{
+				string workingDirectory = Environment.CurrentDirectory;
+				string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
+
+				DateTime dt = DateTime.Now;
+				string filePath = projectDirectory + "/" + "myOwnWebServer.log";
+
+				string formattedMsg = dt.ToString() + logType + " " + message;
+				using (StreamWriter sw = File.AppendText(filePath))
+				{
+					sw.WriteLine(formattedMsg);
+				}
+			}
+			catch (Exception ex)
+			{
+				ServerUI.displayServerMsg(ex.Message);
+			}
+		}
+
 		/// <summary>
 		/// Logs the File with an [START] bracket after Date
 		/// </summary>
-		/// <param name="msg"></param>
+		/// <param name="msg">Message that needs to be logged</param>
 		public static void StartLog(string msg)
 		{
 
-			string workingDirectory = Environment.CurrentDirectory;
-			string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
-
-			string filePath = projectDirectory + "/" + "myOwnWebServer.log";
-
-			DateTime dt = DateTime.Now;
-			string formattedMsg = dt.ToString() + "[START] " + msg;
-			using (StreamWriter sw = File.AppendText(filePath))
-			{
-				sw.WriteLine(formattedMsg);
-			}
+			WriteLog(msg, "[Start]");
 
 		}
 
@@ -40,77 +57,36 @@ namespace A05WDD
 		public static void NormalLog(string msg)
 		{
 
-			string workingDirectory = Environment.CurrentDirectory;
-			string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
-
-			string filePath = projectDirectory + "/" + "myOwnWebServer.log";
-
-			DateTime dt = DateTime.Now;
-			string formattedMsg = dt.ToString() + " " +  msg;
-			using (StreamWriter sw = File.AppendText(filePath))
-			{
-				sw.WriteLine(formattedMsg);
-			}
-		
+			WriteLog(msg, "");
 
 		}
 
 		/// <summary>
 		/// Logs the File with an [REQUEST] bracket after Date
 		/// </summary>
-		/// <param name="msg"></param>
+		/// <param name="msg">Message that needs to be logged</param>
 		public static void RequestLog(string msg)
 		{
-			string workingDirectory = Environment.CurrentDirectory;
-			string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
-
-			string filePath = projectDirectory + "/" + "myOwnWebServer.log";
-
-			DateTime dt = DateTime.Now;
-			string formattedMsg = dt.ToString() + "[REQUEST] " + msg;
-			using (StreamWriter sw = File.AppendText(filePath))
-			{
-				sw.WriteLine(formattedMsg);
-			}
+			WriteLog(msg, "[REQUEST]");
 		}
 
 		/// <summary>
 		/// Logs the File with an [RESPONSE] bracket after Date
 		/// </summary>
-		/// <param name="msg"></param>
+		/// <param name="msg">Message that needs to be logged</param>
 		public static void ResponseLog(string msg) {
 
-			string workingDirectory = Environment.CurrentDirectory;
-			string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
-
-			string filePath = projectDirectory + "/" + "myOwnWebServer.log";
-
-			DateTime dt = DateTime.Now;
-			string formattedMsg = dt.ToString() + "[RESPONSE] " + msg;
-			using (StreamWriter sw = File.AppendText(filePath))
-			{
-				sw.WriteLine(formattedMsg);
-			}
+			WriteLog(msg, "[RESPONSE]");
 		}
 
 		/// <summary>
 		/// Logs the File with an [ERROR] bracket after Date
 		/// </summary>
-		/// <param name="msg"></param>
+		/// <param name="msg">Message that needs to be logged</param>
 		public static void ErrorLog(string msg)
 		{
 
-			string workingDirectory = Environment.CurrentDirectory;
-			string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
-
-			string filePath = projectDirectory + "/" + "myOwnWebServer.log";
-
-			DateTime dt = DateTime.Now;
-			string formattedMsg = dt.ToString() + "[ERROR] " + msg;
-			using (StreamWriter sw = File.AppendText(filePath))
-			{
-				sw.WriteLine(formattedMsg);
-			}
+			WriteLog(msg, "[ERROR]");
 		}
 
 	}
